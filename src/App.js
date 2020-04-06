bd9489b83e86d95f2065b44aad9cce2752438125
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2020-04-06 13:11:31 +0300

    update

--- src/App.js
+++ src/App.js
@@ -9,31 +9,39 @@ import MENUITEMS_QUERY from "./queries/menuItems";
 import Query from "./components/Query";
 import PublicationTest from "./components/PublicationTest";
 import Header from "./components/Header";
+import { ApolloProvider } from "react-apollo";
+import client from "./utils/apolloClient";
+import { Provider } from "react-redux";
+import Store from "./Store";
 
 function App() {
   //Feeds header and Footer elements to the respective components from the API
   return (
-    <MuiThemeProvider theme={Theme}>
-      <div className="App">
-        <CssBaseline />
-        <Grid container spacing={0}>
-          <Grid item xs={12}>
-            <Query query={HEADER_QUERY} id={null}>
-              {({ data: { header } }) => {
-                return <Header header={header} />;
-              }}
-            </Query>
-          </Grid>
-          <Grid item xs={12}>
-            <Router>
-              <p>hello there</p>
-              <PublicationTest />
-            </Router>
-          </Grid>
-          <Grid item xs={12}></Grid>
-        </Grid>
-      </div>
-    </MuiThemeProvider>
+    <ApolloProvider client={client}>
+      <Provider store={Store}>
+        <MuiThemeProvider theme={Theme}>
+          <div className="App" data-testid="app">
+            <CssBaseline />
+            <Grid container spacing={0}>
+              <Grid item xs={12}>
+                <Query query={HEADER_QUERY} id={null}>
+                  {({ data: { header } }) => {
+                    return <Header header={header} />;
+                  }}
+                </Query>
+              </Grid>
+              <Grid item xs={12}>
+                <Router>
+                  <p>hello there</p>
+                  <PublicationTest />
+                </Router>
+              </Grid>
+              <Grid item xs={12}></Grid>
+            </Grid>
+          </div>
+        </MuiThemeProvider>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ApolloProvider } from "react-apollo";
-import client from "./utils/apolloClient";
-import { Provider } from "react-redux";
-import Store from "./Store";
 
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Provider store={Store}>
-      <App />
-    </Provider>
-  </ApolloProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
